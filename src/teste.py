74844d802a776d544920fb9c0183164e20ff04e7
Pedro Henrique <royashiauxph@gmail.com>
2025-02-17 15:02:18 -0300

    graph code

--- src/teste.py
+++ src/teste.py
@@ -1,5 +1,7 @@
 import random
 from pysat.solvers import Glucose4
+import matplotlib.pyplot as plt
+
 
 def gerarClausula(n, m, k):
     clausulas = []
@@ -14,8 +16,6 @@ def gerarClausula(n, m, k):
                 clausula.append(valor * -1)
         if clausula not in clausulas:
              clausulas.append(clausula)
-
-
     return clausulas
 
 
@@ -31,19 +31,43 @@ def solver(clausulas):
     return satisfazivel, tempo
 
 
+
+def gerarGrafico(xvalores, yvalores, xlabel, ylabel, label, title, savefig):
+
+    plt.figure(figsize=(10,6))
+    for x, y in zip(xvalores, yvalores):
+        plt.plot(x, y, label= label)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.grid(True)
+    plt.legend()
+    plt.savefig(f'{savefig}.png')
+    plt.close()
+
+
+# gerarGrafico([[1234, 1234], [34, 56]], [[10, 20], [70, 80]], 'pedro', 'dougras', 'cu', 'teste', 'tesss')
+
+
 ksat = [3, 5]
-variaveis = [50, 100, 150]
+variaveis = [50, 70]
 instancias = 10
 
 for x in ksat:
     alpha_inicio = 1.0
     alpha_fim = 10.0 if x == 3 else 20.0
-    alpha_passos = 0.1
+    alpha_passos = 0.5
     
     num_points = int((alpha_fim - alpha_inicio) / alpha_passos) + 1
     alphas = [alpha_inicio + i * alpha_passos 
               for i in range(num_points)]
     
+    xvalues = []
+    yvalues = []
+    xvalues.append(alphas)
+    probabilidades = []
+    
+
     for y in variaveis:
         print(f"Processing k={x}, n={y}")
 
@@ -54,8 +78,6 @@ for x in ksat:
 
             quant_satisfazivel = 0
             tempo_total = 0.0
-
-
 
 
             for z in range(instancias):
@@ -70,3 +92,8 @@ for x in ksat:
             prob = quant_satisfazivel / instancias
             avg = tempo_total / instancias
             print(f"Alpha: {alpha:.2f}, Prob: {prob:.2f}, Time: {avg:.2f}s")
+            probabilidades.append(prob)
+
+    yvalues.append(probabilidades)
+
+gerarGrafico(xvalues, yvalues, 'teste', 'cu2', 'dougras', 'predo', 'testef')
